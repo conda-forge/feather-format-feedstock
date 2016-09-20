b3089bb8f7a8d63e28677510f0352f3e519781d5
Sylvain Corlay <sylvain.corlay@gmail.com>
2016-09-20 15:24:07 +0200

    Skip py<35

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,2 @@
-if "%ARCH%" == "32" (set PLATFORM=x86) else (set PLATFORM=x64)
-call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" %PLATFORM%
-set DISTUTILS_USE_SDK=1
-set MSSdk=1
 "%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
-
